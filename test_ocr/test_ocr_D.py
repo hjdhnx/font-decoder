52fe1af0ca3a8e1d9d31f31579eb73f4ba4bdb34
晚风拂柳颜 <434857005@qq.com>
2024-10-11 03:09:34 +0800

    发布1.1版本,编译两个版本的ocr对应的exe

--- test_ocr/test_ocr_D.py
+++ test_ocr/test_ocr_D.py
@@ -5,16 +5,21 @@
 # Author's Blog: https://blog.csdn.net/qq_32394351
 # Date  : 2024/10/11
 
+from PIL import Image
 from ddddocr import DdddOcr
 import unittest
 import string
 import easyocr
 import pytesseract
-import muggle_ocr
+# import muggle_ocr
+from pyocr import pyocr
 
 ocr = DdddOcr(show_ad=False, beta=True)
 reader = easyocr.Reader(['en'], gpu=True)
-sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.OCR)
+# sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.OCR)
+# 创建OCR工具
+tool = pyocr.get_available_tools()[0]
+
 
 class TestOcr(unittest.TestCase):
     def test_ocr_d(self):
@@ -29,8 +34,10 @@ class TestOcr(unittest.TestCase):
                 print('ret1:', ret1)
                 ret2 = pytesseract.image_to_string('test.png')
                 print('ret2:', ret2)
-                ret3 = sdk.predict(image_bytes=f.read())
-                print('ret3:',ret3)
+                # ret3 = sdk.predict(image_bytes=f.read())
+                # print('ret3:',ret3)
+                ret3 = tool.image_to_string(Image.open('test.png'), lang='eng')
+                print('ret3:', ret3)
             self.assertEqual(ret, 'D')
 
 
